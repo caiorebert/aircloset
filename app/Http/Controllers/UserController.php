53cd4b4ebf293aa6bb4f1521e76efc75b6092351
Caio Rebert <caiorebert070802.cr@gmail.com>
2022-08-14 23:23:18 -0300

    Retirada de id definido para teste

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,6 @@ class UserController extends Controller
 
     public function updateDados(Request $request){
         $id = auth()->user()->id;
-        $id = 1;
         $user = User::find($id);
         $inputs = $request->input('values');
         foreach ($inputs as $key => $value) {
